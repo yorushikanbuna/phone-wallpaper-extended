89a968eb1d14098df36eacecdb914773c1d5f6dc
yorushikanbuna <yorushikanbuna@users.noreply.github.com>
2026-06-22 16:37:02 +0800

    Add allprojects repositories for dependency resolution

--- android-app/build.gradle.kts
+++ android-app/build.gradle.kts
@@ -2,3 +2,10 @@ plugins {
     id("com.android.application") version "8.2.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.20" apply false
 }
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
